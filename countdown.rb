317909ee0fbf928c88bf26b3acddc1ad2196c42a
Phillip DiPhillipo <diphillipophillip@gmail.com>
2018-10-20 21:11:57 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,15 @@
-#write your code here
 
-def countdown
+
+def countdown(number)
+  x = 10 
+  while x > 0 
+  puts "#{x} SECOND(S)!"
+  x -= 1
+end
+return "HAPPY NEW YEAR!"
+end
+
+
+def countdown_with_sleep(number)
+  sleep 5
 end
